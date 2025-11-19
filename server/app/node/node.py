54f6259b1f4b56ca238a33d95e538b9c5650eb90
3nd3r1 <ender.spam.guru@gmail.com>
2025-11-19 15:13:44 +0200

    feat(node): init raft instance in the node

--- server/app/node/node.py
+++ server/app/node/node.py
@@ -1,16 +1,19 @@
 import asyncio
 
+from app.raft.consensus import RaftConsensus
+
 
 class Node:
     def __init__(self, node_id: str):
         self.node_id = node_id
-
+        self.raft = RaftConsensus(node_id)
         self.background_tasks = []
 
     async def start(self):
         print(f"Node {self.node_id} starting up...")
 
-        # TODO: Start Raft consensus
+        # Initialize Raft consensus
+        print(f"Node {self.node_id} initializing Raft consensus (state: {self.raft.state.value})")
         # TODO: Join cluster if peers exist
         # TODO: Start background heartbeat/election tasks
 
@@ -18,6 +21,9 @@ class Node:
 
     async def stop(self):
         print(f"Node {self.node_id} shutting down...")
+
+        # Stop Raft consensus
+        print(f"Node {self.node_id} stopping Raft consensus")
 
         # Cancel all background tasks
         for task in self.background_tasks:
