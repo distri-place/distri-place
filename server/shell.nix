a160a3d3a9ec608341cc21f2b0c12949f112aedf
3nd3r1 <ender.spam.guru@gmail.com>
2025-12-03 13:53:11 +0200

    chore: fix the server shell.nix

--- server/shell.nix
+++ server/shell.nix
@@ -4,15 +4,13 @@
 
 pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python311.withPackages (
-      ps: with ps; [
-        grpcio-tools
-      ]
-    ))
+    python311
     gnumake
+    stdenv.cc.cc.lib
   ];
 
   shellHook = ''
+    export LD_LIBRARY_PATH=${pkgs.stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
     if [[ -d venv ]]; then
         source venv/bin/activate
     else
